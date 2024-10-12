1b0d134b0ec6e18d8049e7d5facfccddfd9e8a11
xjasxer <99921006+xjasxer@users.noreply.github.com>
2024-10-12 20:28:36 +0000

    pin png hinzugefügt / karten nav hinzugefügt

--- index.php
+++ index.php
@@ -1,11 +1,37 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="de">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Die Herrschaftsformen</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     
+
+
+<div id="nav-karte">
+
+<h5>Was möchtest du angezeigt bekommen?</h4>
+
+<div id="nav-formen">
+<h4>Staatsformen:</h5>
+
+
+<input type="checkbox" id="mo" name="1" value="1">
+<label for="1">Monarchie</label><br>
+
+<input type="checkbox" id="re" name="2" value="2">
+  <label for="2">Republik</label> <br>
+  
+  <input type="checkbox" id="dik" name="3" value="3">
+  <label for="3">Diktatur</label> <br>
+
+
+
+</div>
+
+
+
 </body>
 </html>
