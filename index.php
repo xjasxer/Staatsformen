4cb36a992375fa8e43f724e3aec99bd49eb533db
xjasxer <99921006+xjasxer@users.noreply.github.com>
2024-10-14 09:03:31 +0000

    js code für info text

--- index.php
+++ index.php
@@ -5,9 +5,37 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Die Herrschaftsformen</title>
     <link rel="stylesheet" href="style.css">
+    <script src="js.js"></script>
 </head>
 <body>
     
+<div id="nav-infos">
+  
+<h4>Informationen über die Stattsformen</h4>
+
+<input type="button" value="Monarchie"
+ data-title="Monarchie"
+ data-beschreibung="becshriebunf"
+ data-beschreibung2="2222"
+ id="info1"
+ onclick="info1()">
+
+
+
+
+
+<input type="button" value="Republik">
+<input type="button" value="Diktatur">
+
+</div>
+
+<div id="info">
+  <h1 id="info-header">hh</h1>
+  <p id="info-beschreibung">z</p>
+  <p id="info-beschreibung2">z</p>
+</div>
+
+
 
 
 <div id="nav-karte">
@@ -31,23 +59,6 @@
 
 </div>
 
-<div id="nav-infos">
-  
-<h4>Informationen über die Stattsformen</h4>
-
-<input type="button" value="Monarchie">
-<input type="button" value="Republik">
-<input type="button" value="Diktatur">
-
-</div>
-
-
-
-<?php 
-
-
-
-?>
 
 
 
--- js.js
+++ js.js
@@ -1,1 +1,7 @@
-  
+  function info1() {
+var button = document.getElementById('info1')
+
+document.getElementById('info-header').textContent = button.getAttribute("data-title")
+
+  }
+   
